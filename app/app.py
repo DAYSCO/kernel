9a3f5a6131497ef2a5a063713a7848b489af7bcc
waheedamir <waheedamir2@gmail.com>
2021-10-26 12:51:35 +0500

    Code moved to new repo.

--- app/app.py
+++ app/app.py
@@ -1,26 +1,28 @@
+import os
 import sys
 from flask_api import FlaskAPI
 from .routes import app_bp
 
 
-from opentelemetry import trace
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleSpanProcessor,
-)
-
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
-
-
 app = FlaskAPI(__name__)
 app.register_blueprint(app_bp)
 
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
 
-tracer = trace.get_tracer(__name__)
-tracer.start_as_current_span('app-request')
+if not os.environ.get('TESTING', 'FALSE') == 'TRUE':
+    from opentelemetry import trace
+    from opentelemetry.instrumentation.flask import FlaskInstrumentor
+    from opentelemetry.instrumentation.requests import RequestsInstrumentor
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import (
+        ConsoleSpanExporter,
+        SimpleSpanProcessor,
+    )
+
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+
+    FlaskInstrumentor().instrument_app(app)
+    RequestsInstrumentor().instrument()
+
+    tracer = trace.get_tracer(__name__)
+    tracer.start_as_current_span('app-request')
--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -1,17 +1,33 @@
+status_codes = {
+    "IDNotFoundError": 401,
+    "MissingHeaderAttributeError": 402,
+    "PayloadError": 403,
+    "GeneralError": 404,
+    "InvalidDataTypeError": 405,
+    "MissingPayloadValueError": 406,
+    "DuplicateNameError": 407,
+    "FileParseError": 408,
+}
+
+
 class IDNotFoundError(Exception):
-    def __init__(self, uid, message="id does not exist for any existing working files."):
+    def __init__(self, uid,
+                 message="id does not exist for any existing working files."):
         self.uid = uid
         self.message = message
+        self.status_code = status_codes[self.__class__.__name__]
         super().__init__(self.message)
 
     def __str__(self):
         return f'({self.uid}) -> {self.message}'
+
 
 class PayloadError(Exception):
     def __init__(self, key, value, message="error parsing payload"):
         self.key = key
         self.value = value
         self.message = message
+        self.status_code = status_codes[self.__class__.__name__]
         super().__init__(self.message)
 
     def __str__(self):
@@ -19,11 +35,60 @@ class PayloadError(Exception):
 
 
 class InvalidDataTypeError(Exception):
-    def __init__(self, column, dataType, message="invalid data type for column"):
+    def __init__(self, column, data_type,
+                 message="invalid data type for column"):
         self.column = column
         self.message = message
-        self.dataType = dataType
+        self.dataType = data_type
+        self.status_code = status_codes[self.__class__.__name__]
         super().__init__(self.message)
 
     def __str__(self):
         return f'({self.dataType}) -> {self.message} "{self.column}"'
+
+
+class MissingHeaderAttributeError(Exception):
+    def __init__(self, param, message="missing from headers request"):
+        self.message = message
+        self.param = param
+        self.status_code = status_codes[self.__class__.__name__]
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'({self.param}) -> {self.message}'
+
+
+class MissingPayloadValueError(Exception):
+    def __init__(self, param,
+                 message="value is missing from payload of request"):
+        self.message = message
+        self.param = param
+        self.status_code = status_codes[self.__class__.__name__]
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'({self.param}) -> {self.message}'
+
+
+class DuplicateNameError(Exception):
+    def __init__(self, column,
+                 message="can not duplicate the name"):
+        self.column = column
+        self.message = message
+        self.status_code = status_codes[self.__class__.__name__]
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'({self.column}) -> {self.message}'
+
+
+class FileParseError(Exception):
+    def __init__(self, file_name,
+                 message="Unable to parse the file."):
+        self.file_name = file_name
+        self.message = message
+        self.status_code = status_codes[self.__class__.__name__]
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'({self.file_name}) -> {self.message}'
